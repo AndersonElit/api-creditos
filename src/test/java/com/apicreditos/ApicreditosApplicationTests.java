b32f4d841f09ddfe80c6ae1394d1943688e95d57
Anderson Elit Arrieta Rodrig <rodriarri@gmail.com>
2023-02-11 19:31:28 -0500

    creacion poryecto

---
+++ src/test/java/com/apicreditos/ApicreditosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apicreditos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApicreditosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
