86f890650daf2e9ed544261e19a878e1ab4b494a
AndersonElit <rodriarri@gmail.com>
2023-02-13 00:51:42 -0500

    ajuste vinculacion.

--- domain/vinculacion/src/main/java/com/apicreditos/entities/Vinculacion.java
+++ domain/vinculacion/src/main/java/com/apicreditos/entities/Vinculacion.java
@@ -3,16 +3,20 @@ package com.apicreditos.entities;
 import com.apicreditos.AggregateRoot;
 import com.apicreditos.values.VinculacionId;
 
+import java.time.LocalDate;
+
 public class Vinculacion extends AggregateRoot<VinculacionId> {
     protected Cliente cliente;
     protected Asesor asesor;
     protected Oficina oficina;
+    protected LocalDate fechaViculacion;
 
     public Vinculacion(VinculacionId id, Cliente cliente, Asesor asesor, Oficina oficina) {
         super(id);
         this.cliente = cliente;
         this.asesor = asesor;
         this.oficina = oficina;
+        this.fechaViculacion = LocalDate.now();
         crearCliente(cliente);
         asignarAsesor(asesor);
         asignarOficina(oficina);
